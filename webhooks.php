8b7f01bae5ec018bf79d5893bec9c464dcf4b292
Tor-Zaa <tor4_@hotmail.com>
2019-06-04 15:01:03 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -50,12 +50,3 @@ if (!is_null($events['events'])) {
 }
 echo "OK";
 
-require "vendor/autoload.php";
-$access_token = 'db7xk6drf1c/SnbXZK5El4omIzq2V2p3WYsYW7Iw5d5GARqf7kmG8yjDqnavITxZwaf1UEXQlSSu3o6+fK3l1wMPXWWczg3iM8ii864M0fpUXASRTuZd7XzuMmOpSauejmk/k7F9T+XviDJ+M+MFDwdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '6342109af7f5467cf90cd3b1342ca8cd';
-$idPush = 'U254d7bfb6c277735e5e4bd179baa23fd'
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($idPush, $textMessageBuilder);
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
